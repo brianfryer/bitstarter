9c7afc5c456b72d4082753020870ca0b8660fb8d
Ubuntu <ubuntu@ip-10-243-93-85.ec2.internal>
2013-07-14 22:47:43 +0000

    Added index.html; Updated web.js to output contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var index = fs.readFileSync('index.html'),
+    buf   = new Buffer(index);
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
